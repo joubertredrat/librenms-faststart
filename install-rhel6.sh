6d828adf8bf4a08e6c1c3e2eac91c418551be0ca
Joubert RedRat <me+github@redrat.com.br>
2015-05-30 15:17:59 -0300

    Finished install script for RHEL 6 or CentOS 6

--- install-rhel6.sh
+++ install-rhel6.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 #
 # Script to install LibreNMS with default configurations, providing to the user an initial contact with the tool.
-# Supported OS: Ubuntu 14.04 LTS, Ubuntu 12.04 LTS
+# Supported OS: 
 #
-# Based on http://docs.librenms.org/Installation/Installation-%28Debian-Ubuntu%29/
+# Based on http://docs.librenms.org/Installation/Installation-%28RHEL-CentOS%29/
 # Author Joubert RedRat
-
-export DEBIAN_FRONTEND=noninteractive
 
 random_password() {
     MATRIX='0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'
@@ -26,16 +24,16 @@ function check_root {
 }
 
 function check_os {
-    if [ -e '/etc/redhat-release' ]; then
-        echo 'Error: sorry, this installer works only on Debian or Ubuntu'
-        exit 1
-    fi  
+	if [ ! -e '/etc/redhat-release' ]; then
+	    echo 'Error: sorry, this installer works only on RHEL and CentOS'
+	    exit 1
+	fi
 }
 
 function check_installed {   
     tmpfile=$(mktemp -p /tmp)
-    dpkg --get-selections > $tmpfile
-    for pkg in mysql-server apache2 php5; do
+    rpm -qa > $tmpfile
+    for pkg in mysql-server httpd php5; do
         if [ ! -z "$(grep $pkg $tmpfile)" ]; then
             conflicts="$pkg $conflicts"
         fi
@@ -52,15 +50,15 @@ function check_installed {
 function install_mysql {
     mysqlrootpassword=$(random_password)
     mysqllibrepassword=$(random_password)
-    apt-get install -y mysql-server mysql-client mysql-common
-    service mysql stop > /dev/null 2>&1
+    yum install -y mysql mysql-server
+    /etc/init.d/mysqld stop > /dev/null 2>&1
 
-    /usr/sbin/mysqld --skip-grant-tables --skip-networking &
+    /usr/bin/mysqld_safe --skip-grant-tables --skip-networking &
 
-    echo "UPDATE user SET Password=PASSWORD('$mysqlrootpassword') WHERE User='root'; FLUSH PRIVILEGES; exit;" | mysql -u root mysql
+    echo "UPDATE user SET Password=PASSWORD('$mysqlrootpassword') WHERE User='root'; FLUSH PRIVILEGES;" | mysql -u root mysql
 
-    service mysql stop > /dev/null 2>&1
-    service mysql start
+    /etc/init.d/mysqld stop > /dev/null 2>&1
+    /etc/init.d/mysqld start
 
     echo "CREATE DATABASE librenms;
             GRANT ALL PRIVILEGES ON librenms.*
@@ -70,85 +68,68 @@ function install_mysql {
 }
 
 function install_dependences {
-    apt-get update
-    apt-get upgrade -y
-    apt-get install -y bsdutils curl fping git graphviz imagemagick mtr-tiny nmap python-mysqldb rrdtool whois
+	yum update -y
+	rpm -Uvh http://download.fedoraproject.org/pub/epel/6/i386/epel-release-6-8.noarch.rpm
+	yum install -y curl fping git graphviz ImageMagick jwhois mtr MySQL-python nmap rrdtool vixie-cron
 }
 
 function install_webserver {
-    apt-get install -y apache2 libapache2-mod-php5 php5 php5-cli php5-curl php5-gd php5-json php5-mcrypt php5-mysql php-pear php5-snmp php-net-ipv4 php-net-ipv6
+    yum install -y httpd php php-cli php-curl php-gd php-mcrypt php-mysql php-pear php-snmp graphviz-php
+    pear install Net_IPv4-1.3.4
+    pear install Net_IPv6-1.2.2b2
 
-    if [[ $(apache2 -v | head -n 1) =~ 'Apache/2.4' ]]; then
-        echo '<VirtualHost *:80>
-                DocumentRoot /opt/librenms/html/
-                ServerName localhost.localdomain
-                CustomLog /opt/librenms/logs/access_log combined
-                ErrorLog /opt/librenms/logs/error_log
-                AllowEncodedSlashes On
-                <Directory "/opt/librenms/html/">
-                    AllowOverride All
-                    Options FollowSymLinks MultiViews
-                    Require all granted
-                </Directory>
-            </VirtualHost>' > /etc/apache2/sites-available/librenms.conf
-        a2dissite 000-default            
-    else        
-        echo '<VirtualHost *:80>
-                DocumentRoot /opt/librenms/html/
-                ServerName localhost.localdomain
-                CustomLog /opt/librenms/logs/access_log combined
-                ErrorLog /opt/librenms/logs/error_log
-                AllowEncodedSlashes On
-                <Directory "/opt/librenms/html/">
-                    AllowOverride All
-                    Options FollowSymLinks MultiViews
-                </Directory>
-            </VirtualHost>' > /etc/apache2/sites-available/librenms
-        a2dissite default            
-    fi
-
-    php5enmod mcrypt
-    a2enmod rewrite  
-    a2ensite librenms
-    service apache2 stop > /dev/null 2>&1
-    service apache2 start 
+    echo '<VirtualHost *:80>
+            DocumentRoot /opt/librenms/html/
+            ServerName localhost.localdomain
+            CustomLog /opt/librenms/logs/access_log combined
+            ErrorLog /opt/librenms/logs/error_log
+            AllowEncodedSlashes On
+            <Directory "/opt/librenms/html/">
+                AllowOverride All
+                Options FollowSymLinks MultiViews
+            </Directory>
+        </VirtualHost>' > /etc/httpd/conf.d/librenms.conf
+          
+    /etc/init.d/httpd stop > /dev/null 2>&1
+    /etc/init.d/httpd start 
 }
 
 function install_snmp {
-    apt-get install -y snmp snmpd
+   	yum install -y net-snmp net-snmp-utils
     rm /etc/snmp/snmpd.conf
     echo 'rocommunity public 127.0.0.1' > /etc/snmp/snmpd.conf
-    service snmpd stop > /dev/null 2>&1
-    service snmpd start
+    /etc/init.d/snmpd stop > /dev/null 2>&1
+    /etc/init.d/snmpd start
 }
 
 function install_librenms {
     useradd librenms -d /opt/librenms -M -r
-    usermod -a -G librenms www-data
+    usermod -a -G librenms apache
     cd /opt
     git clone https://github.com/librenms/librenms.git librenms
     cd /opt/librenms
     mkdir rrd logs
-    chown www-data. logs
+    chown apache. logs
     chmod 775 rrd
     chown librenms. rrd
 
     cp /opt/librenms/config.php.default /opt/librenms/config.php
     sed -i 's/USERNAME/librenms/g' /opt/librenms/config.php
     sed -i "s/PASSWORD/$mysqllibrepassword/g" /opt/librenms/config.php
+    echo '$config["fping"] = "/usr/sbin/fping";' >> /opt/librenms/config.php
 
     cp /opt/librenms/librenms.nonroot.cron /etc/cron.d/librenms
 
     librepassword=$(random_password)
 
-    /usr/bin/php5 build-base.php
-    /usr/bin/php5 addhost.php localhost public v2c
-    /usr/bin/php5 adduser.php admin $librepassword 10
-    /usr/bin/php5 discovery.php -h all
-    /usr/bin/php5 poller.php -h all
+    /usr/bin/php build-base.php
+    /usr/bin/php addhost.php localhost public v2c
+    /usr/bin/php adduser.php admin $librepassword 10
+    /usr/bin/php discovery.php -h all
+    /usr/bin/php poller.php -h all
 
-    service apache2 stop > /dev/null 2>&1
-    service apache2 start 
+    /etc/init.d/httpd stop > /dev/null 2>&1
+    /etc/init.d/httpd start 
 }
 
 function its_done {
